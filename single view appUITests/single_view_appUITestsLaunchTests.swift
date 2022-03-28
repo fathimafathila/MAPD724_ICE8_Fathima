a5eba6501fff6c1f1e43fdc48f322f1ef85db68c
Fathima Fathila <fathimafathila@Fathimas-MacBook-Air.local>
2022-03-27 23:43:40 -0400

    Initial Commit

---
+++ single view appUITests/single_view_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  single_view_appUITestsLaunchTests.swift
+//  single view appUITests
+//
+//  Created by Fathima Fathila on 2022-03-27.
+//
+
+import XCTest
+
+class single_view_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
